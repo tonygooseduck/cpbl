313ef6521daef677782db0bd33d43821c3004f66
Iu Chian Tang <tonygooseduck@gmail.com>
2019-05-23 17:38:58 +0800

    User log in function profile.js
    - If access token exists, call /profile api and get user name
    - Change the sign up sign in button on nav to user name

---
+++ public/js/profile.js
@@ -0,0 +1,13 @@
+if (document.cookie) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText);
+            if (data.name) {
+                document.querySelector("#login").innerHTML = `<p class="nav-link active">${data.name}</p>`;
+            }
+        }
+    };
+    xhttp.open("GET", "/profile");
+    xhttp.send();
+}
